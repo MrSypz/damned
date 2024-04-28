4d37f907c8a62a5602843c5233d240393857ef86
Sypztep <chainot@hotmail.co.th>
2024-04-28 15:22:16 +0700

    init

---
+++ src/main/java/sypztep/damned/DamnedClient.java
@@ -0,0 +1,10 @@
+package sypztep.damned;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DamnedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
